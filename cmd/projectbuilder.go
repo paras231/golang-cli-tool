acd0d4828585521c1ef3bcec3c8820e1a9a2ca81
paras231 <sharmaparas553@gmail.com>
2025-03-18 23:25:48 +0530

    updated cli tool to add file path

--- cmd/projectbuilder.go
+++ cmd/projectbuilder.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -14,13 +15,22 @@ var projectBuildercmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// this run function handles the core logic of building CLI
 		folderName := args[0]
+		filePath := filepath.Join("tmp", folderName, "index.js")
 		userName := args[1]
 		fmt.Println(userName, args)
-		err := os.MkdirAll(fmt.Sprintf("tmp/ %s", folderName), os.ModePerm)
+		err := os.MkdirAll(fmt.Sprintf("tmp/%s", folderName), os.ModePerm)
 		if err != nil {
 			fmt.Println("Error creating folder:", err)
 			return
 		}
 		fmt.Println("Folder created successfully:", folderName)
+
+		fmt.Println(filePath)
+		file, err := os.Create(filePath)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file.Close()
 	},
 }
