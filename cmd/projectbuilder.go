350c9e9e6de3d45fea461024d9ce832ffbda6bd2
paras231 <sharmaparas553@gmail.com>
2025-03-18 23:06:38 +0530

    updated cli tool

--- cmd/projectbuilder.go
+++ cmd/projectbuilder.go
@@ -8,11 +8,14 @@ import (
 )
 
 var projectBuildercmd = &cobra.Command{
-	Use:   "new [folder-name]",
+	Use:   "new [folder-name] [user-name]",
 	Short: "Creates a new folder",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(2), // accept two args
 	Run: func(cmd *cobra.Command, args []string) {
+		// this run function handles the core logic of building CLI
 		folderName := args[0]
+		userName := args[1]
+		fmt.Println(userName, args)
 		err := os.MkdirAll(fmt.Sprintf("tmp/ %s", folderName), os.ModePerm)
 		if err != nil {
 			fmt.Println("Error creating folder:", err)
